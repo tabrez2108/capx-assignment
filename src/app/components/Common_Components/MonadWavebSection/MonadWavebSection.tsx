7250aa571b37aea2dd0c6c724a28c40bec30e5d3
Tabrez Anasri <tansari@gammastack.com>
2025-09-11 10:05:40 +0530

    add text with stroke

--- src/app/components/Common_Components/MonadWavebSection/MonadWavebSection.tsx
+++ src/app/components/Common_Components/MonadWavebSection/MonadWavebSection.tsx
@@ -1,21 +1,26 @@
 "use client";
 
 import React from "react";
-import Image from "next/image";
+// import Image from "next/image";
 
-const imgPath = "/assets/images/";
+// const imgPath = "/assets/images/";
+
+const strokeTitleText = "MONAD CARDS";
 
 const MonadWavebSection: React.FC = () => {
   return (
     <section className="w-full">
       <div className="w-full max-w-105 relative mx-auto">
-        <Image 
+        {/* <Image 
           fill
           src={`${imgPath}monad-cards-waveb.webp`} 
           alt="Monad" 
           loading="lazy"
-        className="!w-full !h-auto !static"
-        />
+          className="!w-full !h-auto !static"
+        /> */}
+        <h1 className="stroke-title-text">
+          {strokeTitleText}
+        </h1>
       </div>
 
       <h6 className="text-lightGoldenrodYellow text-sm font-semibold leading-[1.35] italic text-center md:text-lg px-4 mt-2">
